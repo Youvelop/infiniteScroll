94f2f6e2e7675f84b03e710db6dc785397e95526
Elias Molato <ec.molato@gmail.com>
2020-07-08 11:25:49 -0700

    first commit

---
+++ .history/script_20200630180806.js
@@ -0,0 +1,16 @@
+const postContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementsById('filter');
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+  const red = await fetch(
+    `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
---
+++ .history/script_20200630180944.js
@@ -0,0 +1,24 @@
+const postContainer = document.getElementById('posts-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementsById('filter');
+
+let limit = 3;
+let page = 1;
+
+// Fetch posts from API
+async function getPosts() {
+  const red = await fetch(
+    `http://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+
+  const data = await res.json();
+
+  return data;
+}
+
+// Show post in DOM
+async function showPosts() {
+  const posts = await getPosts();
+
+  console.log(posts);
+}
